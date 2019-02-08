6dd270d1fcebb9e44661332aff7dc8f97a532856
jakubczakon <jakub.czakon@neptune.ml>
2019-02-08 19:48:02 +0100

    fixed formatting

--- neptunecontrib/sync/with_json.py
+++ neptunecontrib/sync/with_json.py
@@ -16,12 +16,12 @@
 
 """Syncs json file containg experiment data with Neptune project.
 
-You can run your experiment in any language, create a `.json` file 
+You can run your experiment in any language, create a `.json` file
 that contains your hyper parameters, metrics, tags or properties and send that to Neptune.
 
 Attributes:
-    filepath(str): filepath to the `.json` file that contains experiment data. It can have 
-        ['tags', 'channels', 'properties', 'parameters', 'name'] sections. 
+    filepath(str): filepath to the `.json` file that contains experiment data. It can have
+        ['tags', 'channels', 'properties', 'parameters', 'name'] sections.
         You can pass it either as --filepath or -f.
     project_name(str): Full name of the project. E.g. "neptune-ml/neptune-examples",
         You can pass it either as --project_name or -p.
@@ -29,22 +29,22 @@ Attributes:
 Example:
     Run the experiment and create experiment json in any language.
     For example, lets say your `experiment_data.json` is
-    
-    >>> {"name": "baseline", 
-    >>> "parameters": {"lr": 0.1, 
+
+    >>> {"name": "baseline",
+    >>> "parameters": {"lr": 0.1,
     >>>                "batch_size": 32
-    >>>                }, 
-    >>> "tags": ["base", "solution-1", "pytorch"], 
-    >>> "channels": {"log_loss": {"x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+    >>>                },
+    >>> "tags": ["base", "solution-1", "pytorch"],
+    >>> "channels": {"log_loss": {"x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     >>>                           "y": [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     >>>                           }
-    >>>             }, 
+    >>>             },
     >>> "properties": {"data_version": "version_1"
     >>>               }
     >>> }
 
-    Now you can sync your file with neptune. 
-    
+    Now you can sync your file with neptune.
+
     $ python neptunecontrib.sync.with_json \
       --project_name neptune-ml/neptune-examples \
       --filepath experiment_data.json
@@ -55,35 +55,35 @@ import json
 from subprocess import call
 
 
-def main(args):
-    with open('neptune_sync_main.py', 'w') as main:
-        write_main_content(main, args.filepath)
-        
-    with open('neptune_sync_config.yaml', 'w') as config:
-        write_config_content(config, args.filepath)
-            
+def main(arguments):
+    with open('neptune_sync_main.py', 'w') as main_file:
+        write_main_content(main_file, arguments.filepath)
+
+    with open('neptune_sync_config.yaml', 'w') as config_file:
+        write_config_content(config_file, arguments.filepath)
+
     call('neptune run \
         --exclude neptune_sync_main.py \
         --project {} \
         --config neptune_sync_config.yaml \
-        neptune_sync_main.py'.format(args.project_name), shell=True)
+        neptune_sync_main.py'.format(arguments.project_name), shell=True)
     call('rm neptune_sync_config.yaml neptune_sync_main.py', shell=True)
-    
-    
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filepath')
     parser.add_argument('-p', '--project_name')
     return parser.parse_args()
-    
-    
+
+
 def write_main_content(main_file, experiment_filepath):
     main_content = """
 import neptune
 import json
 
 ctx = neptune.Context()
-    
+
 with open('{}', 'r') as fp:
     data = json.load(fp)
 
@@ -95,28 +95,27 @@ if 'channels' in data.keys():
 if 'properties' in data.keys():
     for name, value in data['properties'].items():
         ctx.properties[name] = value
-            
+
 if 'tags' in data.keys():
     ctx.tags.extend(data['tags'])
 """
-    
+
     main_file.write(main_content.format(experiment_filepath))
-    
-    
+
+
 def write_config_content(config_file, experiment_filepath):
     with open(experiment_filepath, 'r') as fp:
         data = json.load(fp)
-    
+
     if 'name' in data.keys():
         config_file.write('name: {}\n\n'.format(data['name']))
-        
+
     if 'parameters' in data.keys():
         config_file.write('parameters:\n')
         for name, value in data['parameters'].items():
             config_file.write('   {}: {}\n'.format(name, value))
-    
+
+
 if __name__ == '__main__':
-    
     args = parse_args()
     main(args)
-    
