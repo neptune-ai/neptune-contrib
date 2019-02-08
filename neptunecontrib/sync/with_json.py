a9f11d54f9b9d89187629404c032895ba32ab69a
jakubczakon <jakub.czakon@neptune.ml>
2019-02-08 17:21:06 +0100

    added docstrings

--- neptunecontrib/sync/with_json.py
+++ neptunecontrib/sync/with_json.py
@@ -14,11 +14,65 @@
 # limitations under the License.
 #
 
+"""Syncs json file containg experiment data with Neptune project.
+
+You can run your experiment in any language, create a `.json` file 
+that contains your hyper parameters, metrics, tags or properties and send that to Neptune.
+
+Attributes:
+    filepath(str): filepath to the `.json` file that contains experiment data. It can have 
+        ['tags', 'channels', 'properties', 'parameters', 'name'] sections.
+    project_name(str): Full name of the project. E.g. "neptune-ml/neptune-examples"
+
+Example:
+    Run the experiment and create experiment json in any language.
+    For example, lets say your `experiment_data.json` is
+    
+    >>> {"name": "baseline", 
+    >>> "parameters": {"lr": 0.1, 
+    >>>                "batch_size": 32
+    >>>                }, 
+    >>> "tags": ["base", "solution-1", "pytorch"], 
+    >>> "channels": {"log_loss": {"x": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 
+    >>>                           "y": [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    >>>                           }
+    >>>             }, 
+    >>> "properties": {"data_version": "version_1"
+    >>>               }
+    >>> }
+
+    Now you can sync your file with neptune. 
+        $ python neptunecontrib.sync.with_json --project neptune-ml/neptune-examples
+        --filepath experiment_data.json
+"""
+
 import argparse
 import json
 from subprocess import call
 
 
+def main(args):
+    with open('neptune_sync_main.py', 'w') as main:
+        write_main_content(main, args.filepath)
+        
+    with open('neptune_sync_config.yaml', 'w') as config:
+        write_config_content(config, args.filepath)
+            
+    call('neptune run \
+        --exclude neptune_sync_main.py \
+        --project {} \
+        --config neptune_sync_config.yaml \
+        neptune_sync_main.py'.format(args.project_name), shell=True)
+    call('rm neptune_sync_config.yaml neptune_sync_main.py', shell=True)
+    
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--filepath')
+    parser.add_argument('-p', '--project_name')
+    return parser.parse_args()
+    
+    
 def write_main_content(main_file, experiment_filepath):
     main_content = """
 import neptune
@@ -51,26 +105,8 @@ def write_config_content(config_file, experiment_filepath):
     for name, value in data['parameters'].items():
         config_file.write('   {}: {}\n'.format(name, value))
     
-    
-def main(args):
-    with open('neptune_sync_main.py', 'w') as main:
-        write_main_content(main, args.filepath)
-        
-    with open('neptune_sync_config.yaml', 'w') as config:
-        write_config_content(config, args.filepath)
-            
-    call('neptune run \
-        --exclude neptune_sync_main.py \
-        --project {} \
-        --config neptune_sync_config.yaml \
-        neptune_sync_main.py'.format(args.project_name), shell=True)
-    call('rm neptune_sync_config.yaml neptune_sync_main.py', shell=True)
-    
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filepath')
-    parser.add_argument('-p', '--project_name')
-    args = parser.parse_args()
     
+    args = parse_args()
     main(args)
     
