347230edcb533b1e770924947adb60f2241fb288
jakubczakon <czakon.jakub@gmail.com>
2019-07-04 22:15:01 +0200

    added image dir visual snapshoting

--- neptunecontrib/versioning/data.py
+++ neptunecontrib/versioning/data.py
@@ -18,6 +18,10 @@ import hashlib
 
 import boto3
 import neptune
+import numpy as np
+import matplotlib.pyplot as plt
+
+from neptunecontrib.monitoring.utils import send_figure
 
 
 def log_data_version(path, prefix='', experiment=None):
@@ -89,6 +93,43 @@ def log_s3_data_version(bucket_name, path, prefix='', experiment=None):
     _exp.set_property('{}data_version'.format(prefix), _md5_hash_bucket(bucket_name, path))
 
 
+def log_image_dir_snapshots(image_dir, channel_name='image_dir_snapshots', experiment=None, sample=16, seed=1234):
+    """Logs visual snapshot of the directory with image data to Neptune.
+
+    For a given directory with images it logs a sample of images as figure to Neptune.
+    If the `image_dir` specified contains multiple folders it will sample per folder and create
+    multiple figures naming each figure with the folder name.
+    See snapshots per class here https://ui.neptune.ml/jakub-czakon/examples/e/EX-95/channels.
+
+    Args:
+        image_dir(str): path to directory with images.
+        sample(int): number of images that should be sampled for plotting.
+        channel_name(str): name of the neptune channel. Default is 'image_dir_snapshots'.
+        experiment(neptune.experiemnts.Experiment or None): if the data should be logged to a particular
+           neptune experiment it can be passed here. By default it is logged to the current experiment.
+        seed(int): random state for the sampling of images.
+
+    Examples:
+        Initialize Neptune::
+
+            import neptune
+            from neptunecontrib.versioning.data import log_image_dir_snapshots
+            neptune.init('USER_NAME/PROJECT_NAME')
+
+        Log visual snapshot of image directory::
+
+            PATH = 'train_dir/'
+            with neptune.create_experiment():
+                log_image_dir_snapshots(PATH)
+
+    """
+    _exp = experiment if experiment else neptune
+
+    figs = _get_collated_images(image_dir, sample=sample, seed=seed)
+    for fig in figs:
+        send_figure(fig, channel_name=channel_name, experiment=_exp)
+
+
 def _md5_hash_path(path):
     if os.path.isdir(path):
         return _md5_hash_dir(path)
@@ -138,3 +179,66 @@ def _update_hash_md5(hash_md5, filepath):
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return hash_md5
+
+
+def _get_collated_images(image_dir, sample, seed):
+    np.random.seed(seed)
+
+    labels = _get_labels(image_dir)
+    filepaths = _get_filepaths(image_dir)
+
+    figures = []
+    if labels:
+        for label in labels:
+            label_paths = [path for path in filepaths
+                           if path.startswith(os.path.join(image_dir, label))]
+            if len(label_paths) > sample:
+                sample_paths = np.random.choice(label_paths, size=sample, replace=False)
+            else:
+                sample_paths = label_paths
+            collated_image = _get_collated_image(sample_paths, label)
+            figures.append(collated_image)
+    else:
+        if len(filepaths) > sample:
+            sample_paths = np.random.choice(filepaths, size=sample, replace=False)
+        else:
+            sample_paths = filepaths
+        collated_image = _get_collated_image(sample_paths)
+        figures.append(collated_image)
+
+    return figures
+
+
+def _get_labels(dir_path):
+    labels = []
+    for fname in os.listdir(dir_path):
+        if os.path.isdir(os.path.join(dir_path, fname)):
+            labels.append(fname)
+    return labels
+
+
+def _get_filepaths(dir_path):
+    filepaths = []
+    for path, _, files in os.walk(dir_path):
+        for name in files:
+            filepaths.append(os.path.join(path, name))
+    return filepaths
+
+
+def _get_collated_image(filepaths, label=None, figsize=(16, 12), title_size=30):
+    n = len(filepaths)
+    yn, xn = int(np.floor(np.sqrt(n))), int(np.ceil(np.sqrt(n)))
+
+    fig, axs = plt.subplots(yn, xn, figsize=figsize)
+    fig.suptitle(label, fontsize=title_size)
+
+    for i, filepath in enumerate(filepaths):
+        yi, xi = i // xn, i % xn
+        image = plt.imread(filepath)
+        axs[yi, xi].imshow(image)
+        axs[yi, xi].set_xticks([])
+        axs[yi, xi].set_yticks([])
+    plt.tight_layout()
+    fig.subplots_adjust(top=0.92)
+
+    return fig
