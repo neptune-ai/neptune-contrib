ed80946d47e5032fcebce7e1d788a772b8c5af85
Jakub Czakon <czakon.jakub@gmail.com>
2019-10-04 18:32:51 +0200

    updated go-to experiment

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def main():
 
     setup(
         name='neptune-contrib',
-        version='0.13.3',
+        version='0.13.4',
         description='Neptune Python library contributions',
         author='neptune.ml',
         author_email='contact@neptune.ml',
