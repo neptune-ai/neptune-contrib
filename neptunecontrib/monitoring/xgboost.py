8e00d2da4c4b5c7fe27251acad3e4b4a8ef68363
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2020-04-07 17:17:03 +0200

    Updated docstrings

--- neptunecontrib/monitoring/xgboost.py
+++ neptunecontrib/monitoring/xgboost.py
@@ -23,17 +23,19 @@ import xgboost as xgb
 def neptune_callback(log_model=True,
                      log_importance=True,
                      max_num_features=None,
-                     log_tree=(1,),
+                     log_tree=(0,),
                      **kwargs):
     """XGBoost-monitor for Neptune experiments.
 
     This is XGBoost callback that automatically logs training and evaluation metrics, feature importance chart,
     visualized trees and trained Booster to Neptune.
 
+    Make sure you created an experiment before you start XGBoost training using ``neptune.create_experiment()``
+    (`check our docs <https://docs.neptune.ai/neptune-client/docs/project.html
+    #neptune.projects.Project.create_experiment>`_).
+
     Note:
-        Make sure you created an experiment before you start XGBoost training using ``neptune.create_experiment()``
-        (`check our docs <https://docs.neptune.ai/neptune-client/docs/project.html
-        #neptune.projects.Project.create_experiment>`_).
+        Check Neptune documentation for the `full example <https://docs.neptune.ai/integrations/xgboost.html>`_.
 
     Args:
         log_model (:obj:`bool`, optional, default is ``True``):
@@ -42,22 +44,17 @@ def neptune_callback(log_model=True,
             | Log feature importance to Neptune as image after last boosting iteration.
             | Specify number of features using ``max_num_features`` parameter below.
         max_num_features (:obj:`int`, optional, default is ``None``):
-            | Plot top "max_num_features" features on the importance plot.
+            | Plot top ``max_num_features`` features on the importance plot.
             | If ``None``, plot all features.
         log_tree (:obj:`list` of :obj:`int`, optional, default is ``[1,]``):
             | Log specified trees to Neptune as images after last boosting iteration.
             | Default is to log first tree.
         kwargs:
-            | Parametrize XGBoost functions used in this callback:
-              `xgboost.plot_importance <https://xgboost.readthedocs.io/en/latest/python/python_api.html
-              ?highlight=plot_tree#xgboost.plot_importance>`_
-              and `xgboost.to_graphviz <https://xgboost.readthedocs.io/en/latest/python/python_api.html
-              ?highlight=plot_tree#xgboost.to_graphviz>`_.
-
-    Tip:
-        If you use early stopping, make sure to log model, feature importance and trees on your own
-        as Neptune logs these artifacts only after last boosting iteration
-        specified as ``num_boost_round`` in the XGBoost API.
+            Parametrize XGBoost functions used in this callback:
+            `xgboost.plot_importance <https://xgboost.readthedocs.io/en/latest/python/python_api.html
+            ?highlight=plot_tree#xgboost.plot_importance>`_
+            and `xgboost.to_graphviz <https://xgboost.readthedocs.io/en/latest/python/python_api.html
+            ?highlight=plot_tree#xgboost.to_graphviz>`_.
 
     Returns:
         :obj:`callback`, function that you can pass directly to the XGBoost callbacks list, for example to the
@@ -67,11 +64,28 @@ def neptune_callback(log_model=True,
         (`check docs <https://xgboost.readthedocs.io/en/latest/python/python_api.html?highlight=plot_tree
         #xgboost.XGBClassifier.fit>`_).
 
+    Note:
+        If you use early stopping, make sure to log model, feature importance and trees on your own.
+        Neptune logs these artifacts only after last iteration, which you may not reach because of early stop.
+
     Examples:
         .. code:: python3
 
-            # Make sure you have your project set:
-            neptune.init('USERNAME/example-project')
+            # Basic usage in the train function
+            xgb.train(param, dtrain, num_round, watchlist,
+                      callbacks=[neptune_callback()])
+
+            # Use callback in train function, do not log model
+            xgb.train(param, dtrain, num_round, watchlist,
+                      callbacks=[neptune_callback(log_model=False)])
+
+            # Use callback in train function, log five trees
+            xgb.train(param, dtrain, num_round, watchlist,
+                      callbacks=[neptune_callback(log_tree=[0,1,2,3,4])])
+
+            # Use callback in train function, log top 5 features' importance chart
+            xgb.train(param, dtrain, num_round, watchlist,
+                      callbacks=[neptune_callback(max_num_features=5)])
     """
     try:
         neptune.get_experiment()
@@ -88,7 +102,7 @@ def neptune_callback(log_model=True,
         'log_importance must be bool, got {} instead. Check log_importance parameter.'.format(type(log_importance))
     if max_num_features is not None:
         assert isinstance(max_num_features, int),\
-            'max_num_features must be int, got {} instead.' \
+            'max_num_features must be int, got {} instead. ' \
             'Check max_num_features parameter.'.format(type(max_num_features))
     if log_tree is not None:
         if isinstance(log_tree, tuple):
@@ -127,7 +141,5 @@ def neptune_callback(log_model=True,
     return callback
 
 # ToDo docstrings
-# ToDo larges example data
-# ToDO make sure graphviz or neptune errors will not crash exp
 # ToDo Check with CV
 # ToDo Check with sklean API
