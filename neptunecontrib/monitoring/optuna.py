08fa6dc00e6febd1a7715801ca4371bb51743db6
Jakub Czakon <czakon.jakub@gmail.com>
2020-05-19 08:40:12 +0200

    added interactive charts, renamed NeptuneMonitor to NeptuneCallback

--- neptunecontrib/monitoring/optuna.py
+++ neptunecontrib/monitoring/optuna.py
@@ -13,13 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import warnings
 
 import neptune
-from neptunecontrib.monitoring.utils import pickle_and_send_artifact
+
+from neptunecontrib.api import log_chart, pickle_and_log_artifact
 
 
-def NeptuneMonitor(experiment=None):
+def NeptuneCallback(experiment=None):
     """Logs hyperparameter optimization process to Neptune.
+
+    For each iteration it logs run metric and run parameters as well as the best score to date.
 
     Args:
         experiment(`neptune.experiments.Experiment`): Neptune experiment. Default is None.
@@ -30,20 +34,21 @@ def NeptuneMonitor(experiment=None):
             import neptune
             import neptunecontrib.monitoring.optuna as opt_utils
 
-            neptune.init(project_qualified_name='USER_NAME/PROJECT_NAME')
+            neptune.init(api_token='ANONYMOUS',
+                         project_qualified_name='shared/showroom')
             neptune.create_experiment(name='optuna sweep')
 
-            monitor = opt_utils.NeptuneMonitor()
+            neptune_callback = opt_utils.NeptuneCallback()
 
         Run Optuna training passing monitor as callback::
 
             ...
             study = optuna.create_study(direction='maximize')
-            study.optimize(objective, n_trials=100, callbacks=[monitor])
+            study.optimize(objective, n_trials=100, callbacks=[neptune_callback])
 
-        You can explore an example experiment in Neptune https://ui.neptune.ai/jakub-czakon/blog-hpo/e/BLOG-404/charts
+        You can explore an example experiment in Neptune:
+        https://ui.neptune.ai/o/shared/org/showroom/e/SHOW-1016/artifacts
     """
-
     _exp = experiment if experiment else neptune
 
     def monitor(study, trial):
@@ -54,14 +59,18 @@ def NeptuneMonitor(experiment=None):
     return monitor
 
 
-def log_study(study, experiment=None):
+def log_study(study, experiment=None, log_charts=True, params=None):
     """Logs runs results and parameters to neptune.
+
     Logs all hyperparameter optimization results to Neptune. Those include best score ('best_score' channel),
-    best parameters ('best_parameters' property), and the study object itself.
+    best parameters ('best_parameters' property), the study object itself, and interactive optuna charts
+    ('contour', 'parallel_coordinate', 'slice', 'optimization_history').
 
     Args:
         results('optuna.study.Study'): Optuna study object after training is completed.
         experiment(`neptune.experiments.Experiment`): Neptune experiment. Default is None.
+        log_charts('bool'): Whether optuna visualization charts should be logged. By default all charts are logged.
+        params(`list`): List of parameters to be visualized. Default is all parameters.
 
     Examples:
         Initialize neptune_monitor::
@@ -72,19 +81,36 @@ def log_study(study, experiment=None):
             neptune.init(project_qualified_name='USER_NAME/PROJECT_NAME')
             neptune.create_experiment(name='optuna sweep')
 
-            monitor = opt_utils.NeptuneMonitor()
+            neptune_callback = opt_utils.NeptuneCallback()
 
         Run Optuna training passing monitor as callback::
 
             ...
             study = optuna.create_study(direction='maximize')
-            study.optimize(objective, n_trials=100, callbacks=[monitor])
+            study.optimize(objective, n_trials=100, callbacks=[neptune_callback])
             opt_utils.log_study(study)
 
-        You can explore an example experiment in Neptune https://ui.neptune.ai/jakub-czakon/blog-hpo/e/BLOG-404/charts
+        You can explore an example experiment in Neptune:
+        https://ui.neptune.ai/o/shared/org/showroom/e/SHOW-1016/artifacts
      """
+    import optuna.visualization as vis
+
     _exp = experiment if experiment else neptune
 
     _exp.log_metric('best_score', study.best_value)
     _exp.set_property('best_parameters', study.best_params)
-    pickle_and_send_artifact(study, 'study.pkl', experiment=_exp)
+
+    if log_charts:
+        log_chart(name='optimization_history', chart=vis.plot_optimization_history(study), experiment=_exp)
+        log_chart(name='contour', chart=vis.plot_contour(study, params=params), experiment=_exp)
+        log_chart(name='parallel_coordinate', chart=vis.plot_parallel_coordinate(study, params=params), experiment=_exp)
+        log_chart(name='slice', chart=vis.plot_slice(study, params=params), experiment=_exp)
+
+    pickle_and_log_artifact(study, 'study.pkl', experiment=_exp)
+
+
+def NeptuneMonitor(experiment=None):
+    message = """NeptuneMonitor was renamed to NeptuneCallback and will be removed in future releases.
+    """
+    warnings.warn(message)
+    return NeptuneCallback(experiment=experiment)
