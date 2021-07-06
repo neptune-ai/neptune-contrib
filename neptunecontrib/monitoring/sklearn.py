1564934bcc48cb53877f4732b756f41b42052945
Rafal Jankowski <rafal.jankowski@neptune.ai>
2021-07-06 17:56:44 +0200

    Restored old check

--- neptunecontrib/monitoring/sklearn.py
+++ neptunecontrib/monitoring/sklearn.py
@@ -1228,8 +1228,11 @@ def log_silhouette_chart(model, X, experiment=None, **kwargs):
 
 
 def _validate_experiment(experiment):
+    expect_not_a_run(experiment)
+
     if experiment is not None:
-        expect_not_a_run(experiment)
+        if not isinstance(experiment, neptune.experiments.Experiment):
+            ValueError('Passed experiment is not Neptune experiment. Create one by using "create_experiment()"')
     else:
         try:
             experiment = neptune.get_experiment()
